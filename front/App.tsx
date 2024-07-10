d8fde87f6be2e1af11f757fa500aa3a28a74ad84
Bang Uhyeok <0105bang@gmail.com>
2024-07-10 23:17:50 +0900

    flex box

--- front/App.tsx
+++ front/App.tsx
@@ -11,22 +11,35 @@ import {
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>텍스트</Text>
-      <Button
-        title="버튼이름"
-        onPress={() => {
-          console.log('클릭됨!');
-        }}
-      />
+      <View style={styles.inputContainer}>
+        <Text>텍스트</Text>
+        <TextInput style={styles.input} />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text>텍스트</Text>
+        <TextInput style={styles.input} />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'blue',
-    marginHorizontal: 10,
-    marginVertical: 10,
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'black',
+    height: 100,
+    width: 100,
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
