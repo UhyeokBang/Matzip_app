5f22cdeaa0f4a6186b62a7f69deae899fadc4fe8
Bang Uhyeok <0105bang@gmail.com>
2024-07-11 23:36:27 +0900

    이벤트 처리

--- front/App.tsx
+++ front/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   SafeAreaView,
@@ -9,15 +9,21 @@ import {
 } from 'react-native';
 
 function App(): React.JSX.Element {
+  const [name, setName] = useState('');
+
+  const handleChangeInput = (text: string) => {
+    setName(text);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <Text>텍스트</Text>
-        <TextInput style={styles.input} />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text>텍스트</Text>
-        <TextInput style={styles.input} />
+        <Text>이름</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          onChangeText={handleChangeInput}
+        />
       </View>
     </SafeAreaView>
   );
@@ -26,18 +32,16 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
   },
   input: {
     flex: 1,
     borderWidth: 2,
     borderColor: 'black',
-    height: 100,
+    height: 50,
     width: 100,
   },
   inputContainer: {
     flex: 1,
-    backgroundColor: 'red',
     flexDirection: 'row',
     alignItems: 'center',
   },
