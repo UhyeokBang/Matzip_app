1baa7cd59052541906d5a3985b322f1a932b7ea8
Bang Uhyeok <0105bang@gmail.com>
2024-07-14 23:55:59 +0900

    modified wrong Text component path in several screens

--- front/src/screens/calender/CalenderHomeScreen.tsx
+++ front/src/screens/calender/CalenderHomeScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-reanimated/lib/typescript/Animated';
+import {StyleSheet, View, Text} from 'react-native';
 
 function CalenderHomeScreen() {
   return (
